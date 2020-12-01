62590695b2e405672fcee33c572056dd14a5613d
Jiazi Cai <caijiazi777@gmail.com>
2020-12-01 13:46:05 -0800

    added error handlers, created user model, hash pwd

---
+++ auth/src/models/user.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+// An interface describes property for user
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done(null);
+});
+
+const build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc>("User", userSchema);
+
+export { User, build };
