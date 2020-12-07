265ef348a917edb817a8cf37a959ef00cdcc9f55
Jiazi Cai <caijiazi777@gmail.com>
2020-12-06 16:44:35 -0800

    refactor code for payment services, stripe

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,90 @@
+import { OrderStatus } from "@jcticket/common";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { stripe } from "../../stripe";
+import { Payment } from "../../models/payment";
+
+// jest.mock("../../stripe");
+
+it("returns a 404 when order does not exist", async () => {
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({ token: "sddf", orderId: mongoose.Types.ObjectId().toHexString() })
+    .expect(404);
+});
+
+it("returns 401 when order does not belong to user", async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({ token: "sddf", orderId: order.id })
+    .expect(401);
+});
+
+it("returns 400 when purchasing a cancelled order", async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin(userId))
+    .send({ orderId: order.id, token: "2dffg" })
+    .expect(400);
+});
+
+it("returns a 201 with valid inputs", async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+  const price = Math.floor(Math.random() * 100000);
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: userId,
+    version: 0,
+    price,
+    status: OrderStatus.Created,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin(userId))
+    .send({ token: "tok_visa", orderId: order.id })
+    .expect(201);
+
+  const stripeCharges = await stripe.charges.list({ limit: 50 });
+  const stripeCharge = stripeCharges.data.find((charge) => {
+    return charge.amount === price * 100;
+  });
+
+  expect(stripeCharge).toBeDefined();
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+
+  expect(payment).not.toBeNull();
+});
