b60d4e47f598d33ec2c8dcae27198666af4ee32d
Jiazi Cai <caijiazi777@gmail.com>
2020-12-07 23:25:24 -0800

    change commit

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -32,7 +32,7 @@ router.post(
   async (req: Request, res: Response) => {
     const { ticketId } = req.body;
 
-    // Find the ticket the user is trying to order in the database
+    // Find the ticket the user is trying to order in the database!!!
     const ticket = await Ticket.findById(ticketId);
 
     if (!ticket) {
