cccffba2939f805e4b40bcde0c6a32f9cb231e6b
Jiazi Cai <caijiazi777@gmail.com>
2020-12-07 23:27:53 -0800

    clean cached

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Subjects, Publisher, ExpirationCompleteEvent } from "@jcticket/common";
-
-export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
-  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
-}
--- orders/src/events/publishers/order-cancelled-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Subjects, Publisher, OrderCancelledEvent } from "@jcticket/common";
-
-export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
-  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
-}
